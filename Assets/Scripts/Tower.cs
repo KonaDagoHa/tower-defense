d194aa3d583546f71fe8b01025fba429bdbdd504
Khoa Dang Hoang <konadagoha@gmail.com>
2020-12-28 13:28:00 -0500

    Fix CPU spikes with tower by using a sphere trigger rather than overlapsphere()

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -3,38 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: make it so tower linearly rotates towards unit before shooting
+
 public class Tower : MonoBehaviour
 {
     [SerializeField] private LayerMask unitsMask;
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform projectileOrigin;
     [SerializeField] private Transform shooter;
-    private float detectionRadius = 5;
-    private Collider[] unitsToShoot = new Collider[1];
-    private WaitForSeconds shootDelay = new WaitForSeconds(0.01f);
-    private float destoryProjectileAfter = 1;
+    private WaitForSeconds shootDelay = new WaitForSeconds(0.5f);
+    private float destroyProjectileTime = 1;
     private bool canShoot = true;
+    private Vector3 shootDirection = Vector3.forward;
 
-    private void Update()
+    private void OnTriggerStay(Collider other)
     {
-        ShootAtUnits();
+        if (other.gameObject.layer == LayerMask.NameToLayer("Units"))
+        {
+            shootDirection = other.transform.position - shooter.position;
+            shooter.forward = shootDirection;
+            Shoot();
+        }
     }
 
-    private void ShootAtUnits()
+    private void Shoot()
     {
         if (canShoot)
         {
-            if (Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, unitsToShoot, unitsMask) > 0)
-            {
-                Vector3 shootDirection = unitsToShoot[0].transform.position - transform.position;
-                shooter.forward = shootDirection;
-                GameObject projectileGO = Instantiate(projectilePrefab, projectileOrigin);
-                Rigidbody projectileRB = projectileGO.GetComponent<Rigidbody>();
-                projectileRB.AddForce(shootDirection * 15, ForceMode.VelocityChange);
-                canShoot = false;
-                StartCoroutine(ShootCooldown());
-                Destroy(projectileGO, destoryProjectileAfter);
-            }
+            GameObject projectileGO = Instantiate(projectilePrefab, projectileOrigin);
+            Rigidbody projectileRB = projectileGO.GetComponent<Rigidbody>();
+            projectileRB.AddForce(shootDirection * 15, ForceMode.VelocityChange);
+            canShoot = false;
+            StartCoroutine(ShootCooldown());
+            Destroy(projectileGO, destroyProjectileTime);
         }
     }
 
