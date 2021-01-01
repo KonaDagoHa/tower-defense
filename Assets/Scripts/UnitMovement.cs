8c0f472ec79fc7cac6bd04d70864fd89ab9ba370
Khoa Dang Hoang <konadagoha@gmail.com>
2021-01-01 01:09:20 -0500

    Fix unit separation (formerly avoidance) behavior; tweak flow field steering behavior
    Previous version of unit separation didn't account for distance properly because it normalized the awayFromUnit vector;
    Flow field behavior now evaluates from a calculated future position rather than unit's current position

--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -31,14 +31,12 @@ public class UnitMovement : MonoBehaviour
     // unit detection
     private Collider[] unitsDetected = new Collider[8];
     private int numUnitsDetected;
-    private float detectionRadius = 2;
-    private float avoidanceRadius = 2;
-    private float avoidanceFOV = 60; // unit must be within this field of view angle to be avoided
+    private float detectionRadius = 10;
 
     // movement (done in FixedUpdate())
     private Vector3 targetPosition; // position that unit wants to go to
     private float maxVelocity = 5f; // controls how fast unit can move
-    private float maxAcceleration = 10f; // controls how fast unit can reach max speed/velocity
+    private float maxAcceleration = 10; // controls how fast unit can reach max speed/velocity (lower values produce more curved paths; high values more responsive movement)
     private Vector3 currentVelocity;
     private Vector3 currentAcceleration;
 
@@ -46,13 +44,14 @@ public class UnitMovement : MonoBehaviour
 
 
     // steering
-    private float arrivalDistance = 2; // once unit is within this distance of the targetPosition, slow down gradually
-    
+    private float arrivalDistance = 2; // once this unit is within this distance of the targetPosition, slow down gradually
+    private float separationDistance = 10; // other units within this distance will trigger the separation steering behavior
+
     [Serializable]
     public class SteeringWeights
     {
         public float flowField = 1;
-        public float avoidUnits = 1;
+        public float separation = 1;
     }
 
     private void Awake()
@@ -98,17 +97,23 @@ public class UnitMovement : MonoBehaviour
         }
     }
 
+    private bool CheckWithinFOV(Vector3 position, float fov)
+    {
+        // check if provided position is within this unit's FOV (within a cone coming out of unit)
+        Vector3 towardsPosition = position - transform.position;
+        float angleToPosition = Vector3.Angle(transform.forward, towardsPosition);
+        return angleToPosition <= fov;
+    }
+    
     // MOVEMENT BEHAVIORS (STEERING)
 
     private void UpdatePosition()
     {
+        targetPosition = flowField.targetPosition;
         // units move by manipulating their acceleration (not velocity)
-        targetPosition = flowField.targetPosition; // change/delete this later so that targetPosition is not assigned every fixedupdate
         Vector3 towardsTarget = targetPosition - transform.position;
         Vector3 towardsTargetXZ = new Vector3(towardsTarget.x, 0, towardsTarget.z); // ignore the y axis
         
-        // TODO: change this so unit will only go towards targetPosition IF they are not avoiding something
-        // this overwrites avoidance behavior, causing all units to clump together
         float sqrDistance = towardsTargetXZ.sqrMagnitude;
         if (sqrDistance <= arrivalDistance * arrivalDistance) // check if unit is within arrival distance
         {
@@ -118,50 +123,65 @@ public class UnitMovement : MonoBehaviour
         }
         else
         {
-            currentAcceleration = SteeredAcceleration(currentAcceleration);
+            currentAcceleration = SteeredAcceleration();
             currentVelocity += currentAcceleration * Time.deltaTime;
             currentVelocity = Vector3.ClampMagnitude(currentVelocity, maxVelocity);
         }
-        
+
         // move unit according to new acceleration and velocity
         Vector3 positionChange = currentVelocity * Time.deltaTime +
                                  currentAcceleration * (0.5f * Time.deltaTime * Time.deltaTime);
         selfRigidbody.MovePosition(transform.position + positionChange);
     }
-    
-    private Vector3 SteeredAcceleration(Vector3 accelerationToSteer)
+
+    private Vector3 SteeredAcceleration()
     {
         Vector3 steering = Vector3.zero; // change in acceleration
-        steering += FlowFieldSteering(accelerationToSteer) * weights.flowField;
-        steering += AvoidUnitsSteering(accelerationToSteer) * weights.avoidUnits;
-        Vector3 steeredAcceleration = accelerationToSteer + steering;
+        steering += FlowFieldSteering() * weights.flowField;
+        steering += SeparationSteering() * weights.separation;
+        Vector3 steeredAcceleration = currentAcceleration + steering;
         steeredAcceleration = Vector3.ClampMagnitude(steeredAcceleration, maxAcceleration);
 
         // return the steered acceleration (what the acceleration should be after applied steering behaviors)
         return steeredAcceleration;
     }
 
-    // use this for long distance pathfinding
-    private Vector3 FlowFieldSteering(Vector3 accelerationToSteer)
+    private Vector3 ArrivalSteering()
     {
-        Vector3 currentPosition = transform.position;
-        Node currentNode = map.WorldToNode(currentPosition);
-        
-        Vector3 desiredAcceleration;
-        if (currentNode == flowField.targetNode)
+        Vector3 towardsTarget = targetPosition - transform.position;
+        Vector3 towardsTargetXZ = new Vector3(towardsTarget.x, 0, towardsTarget.z); // ignore the y axis
+        float sqrDistance = towardsTargetXZ.sqrMagnitude;
+        if (sqrDistance <= arrivalDistance * arrivalDistance) // unit is within arrival distance (is arriving)
         {
-            // if currentNode is targetNode, move towards targetPosition located inside targetNode (gradually slow down)
-            desiredAcceleration = flowField.targetPosition - currentPosition;
+            currentAcceleration = Vector3.zero; // overwrite acceleration
+            currentVelocity = towardsTargetXZ.normalized * maxVelocity; // go towards target
+            currentVelocity *= Mathf.Sqrt(sqrDistance) / arrivalDistance; // speed decreases as unit approaches targetPosition
+        }
+
+        return Vector3.zero;
+    }
+
+    // use this for long distance pathfinding
+    private Vector3 FlowFieldSteering()
+    {
+        targetPosition = flowField.targetPosition; // change/delete this later
+        Vector3 futurePosition = transform.position + currentVelocity * Time.deltaTime; // TODO: adjust future position by change Time.deltaTime to a higher value in seconds
+        Node futureNode = map.WorldToNode(futurePosition);
+        Vector3 desiredAcceleration;
+        if (futureNode == flowField.targetNode)
+        {
+            // if evaluated node is targetNode, move towards targetPosition located inside targetNode (gradually slow down)
+            desiredAcceleration = targetPosition - futurePosition;
         }
         else
         {
-            // if currentNode is NOT targetNode, follow currentNode's flowDirection (max speed)
-            desiredAcceleration = currentNode.flowDirection;
+            // if evaluated node is NOT targetNode, follow flowDirection (max speed)
+            desiredAcceleration = futureNode.flowDirection;
         }
 
         desiredAcceleration *= maxAcceleration;
         desiredAcceleration = Vector3.ClampMagnitude(desiredAcceleration, maxAcceleration);
-        Vector3 steering = desiredAcceleration - accelerationToSteer;
+        Vector3 steering = desiredAcceleration - currentAcceleration;
         return new Vector3(steering.x, 0, steering.z);
     }
     
@@ -180,12 +200,12 @@ public class UnitMovement : MonoBehaviour
     }
     
     // use this for all pathfinding
-    private Vector3 AvoidUnitsSteering(Vector3 accelerationToSteer)
+    private Vector3 SeparationSteering()
     {
         if (numUnitsDetected > 1)
         {
             Vector3 desiredAcceleration = Vector3.zero;
-            int numUnitsToAvoid = 0;
+            int numUnitsValid = 0;
             for (int i = 0; i < numUnitsDetected; i++)
             {
                 Collider unitCollider = unitsDetected[i];
@@ -196,27 +216,20 @@ public class UnitMovement : MonoBehaviour
                     
                     float awayFromUnitMagnitude = awayFromUnit.magnitude;
                     // if awayFromUnit is not a zero vector AND the unit is within avoidanceRadius
-                    if (awayFromUnitMagnitude != 0 && awayFromUnitMagnitude <= avoidanceRadius)
+                    if (awayFromUnitMagnitude != 0 && awayFromUnitMagnitude <= separationDistance)
                     {
-                        // check if unit is within field of view
-                        Vector3 towardsUnit = -awayFromUnit;
-                        float angleToUnit = Vector3.Angle(transform.forward, towardsUnit);
-                        if (angleToUnit <= avoidanceFOV)
-                        {
-                            numUnitsToAvoid++;
-                            // the closer the unitCollider is, the larger the magnitude of awayFromUnit
-                            awayFromUnit /= awayFromUnitMagnitude;
-                            desiredAcceleration += awayFromUnit;
-                        }
+                        numUnitsValid++;
+                        desiredAcceleration += awayFromUnit;
                     }
                 }
             }
             
-            if (numUnitsToAvoid > 0)
+            if (numUnitsValid > 0)
             {
-                desiredAcceleration /= numUnitsToAvoid;
+                desiredAcceleration /= numUnitsValid; // find average magnitude
                 desiredAcceleration *= maxAcceleration;
-                Vector3 steering = desiredAcceleration - accelerationToSteer;
+                desiredAcceleration = Vector3.ClampMagnitude(desiredAcceleration, maxAcceleration);
+                Vector3 steering = desiredAcceleration - currentAcceleration;
                 // only steer on the xy plane
                 return new Vector3(steering.x, 0, steering.z);
             }
